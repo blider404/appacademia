fece7f91c9dd34c43fe752b4248066a68163bb6d
blider <blider404@protonmail.com>
2025-08-09 16:36:13 -0300

    new file:   menu-app-de-treino/App.js 	modified:   menu-app-de-treino/app.json 	new file:   menu-app-de-treino/assets/adaptive-icon.png 	new file:   menu-app-de-treino/assets/favicon.png 	new file:   menu-app-de-treino/assets/icon.png 	new file:   menu-app-de-treino/assets/splash-icon.png 	new file:   menu-app-de-treino/index.js 	new file:   menu-app-de-treino/my-app/.gitignore 	modified:   menu-app-de-treino/package-lock.json 	modified:   menu-app-de-treino/package.json 	modified:   menu-app-de-treino/src/screens/HomeScreen.js

--- menu-app-de-treino/src/screens/HomeScreen.js
+++ menu-app-de-treino/src/screens/HomeScreen.js
@@ -2,5 +2,23 @@ import React from "react";
 import {View, Text, Flatlist} from 'react-native';
 
 const treinos = [
-    
-]
+    {dia: 'Segunda', grupo: 'Peito e triceps'},
+    {dia: 'Terça', grupo: 'Costas e biceps'},
+    {dia: 'Quarta', grupo: 'Perna'},
+];
+export default function HomeScreen(){
+    return(
+        <View style={{ padding:20 }}>
+            <Text style={{ fontSiza:24, fontWeight:'bold'}}>Treino da Semana</Text>
+            <Flatlist
+                data={treinos}
+                keyExtractor={(item) => item.dia}
+                renderItem={({item}) => (
+                    <View style={{marginVertical: 10}}>
+                        <Text style={{ fontSize: 18}}>{item.dia} - {item.grupo}</Text>
+                    </View>
+                )}
+                />
+        </View>
+    );
+}
